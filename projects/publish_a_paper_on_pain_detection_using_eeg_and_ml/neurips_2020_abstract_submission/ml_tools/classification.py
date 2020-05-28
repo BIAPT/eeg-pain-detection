909cf11dfbd2289bff41380e6bffcfd78b6c0f50
Yacine Mahdid <yacine.mahdid@mail.mcgill.ca>
2020-05-28 02:06:15 -0400

    remove logistic reg

--- projects/publish_a_paper_on_pain_detection_using_eeg_and_ml/neurips_2020_abstract_submission/ml_tools/classification.py
+++ projects/publish_a_paper_on_pain_detection_using_eeg_and_ml/neurips_2020_abstract_submission/ml_tools/classification.py
@@ -170,6 +170,7 @@ def create_gridsearch_pipeline():
         ('clf', DummyEstimator())])  # Placeholder Estimator
 
     # Candidate learning algorithms and their hyperparameters
+    '''
     search_space = [{'clf': [LogisticRegression()],  # Actual Estimator
                      'clf__penalty': ['l2'],
                      'clf__solver': ['lbfgs'],
@@ -182,7 +183,13 @@ def create_gridsearch_pipeline():
 
                     {'clf': [DecisionTreeClassifier()],  # Actual Estimator
                      'clf__criterion': ['gini', 'entropy']}]
+    '''
+    search_space = [{'clf': [SVC()],
+                     'clf__kernel': ['linear'],
+                     'clf__C': [1, 10, 100, 1000]},
 
+                    {'clf': [DecisionTreeClassifier()],  # Actual Estimator
+                     'clf__criterion': ['gini', 'entropy']}]
     # We will try to use as many processor as possible for the gridsearch
     gs = GridSearchCV(pipe, search_space, cv=LeaveOneGroupOut(), n_jobs=-1)
     return gs
