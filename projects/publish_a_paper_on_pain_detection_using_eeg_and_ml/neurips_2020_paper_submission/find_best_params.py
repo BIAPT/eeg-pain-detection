0bd4bf2e04bb2256fe8a15bc804c1ffee04ea6b5
Yacine Mahdid <yacine.mahdid@mail.mcgill.ca>
2020-05-30 00:05:07 -0400

    Modify the find_best_param file to save at right spot

--- projects/publish_a_paper_on_pain_detection_using_eeg_and_ml/neurips_2020_paper_submission/find_best_params.py
+++ projects/publish_a_paper_on_pain_detection_using_eeg_and_ml/neurips_2020_paper_submission/find_best_params.py
@@ -12,24 +12,26 @@ from ml_tools.classification import save_model
 from ml_tools.pre_processing import pre_process
 
 
-
-
 if __name__ == '__main__':
     # Global Experimental Variable
     input_filename = '/lustre03/project/6010672/yacine08/eeg_pain_result/features_all.csv'
-    #input_filename = '/home/yacine/Documents/features_all.csv'
+    output_dir = '/lustre03/project/6010672/yacine08/eeg_pain_result/'
+    gs_filename = output_dir + 'trained_gs.pickle'
+    acc_filename = output_dir + 'accuracies_result.pickle'
+    best_params_filename = output_dir + 'best_params.pickle'
+
     gs = create_gridsearch_pipeline()
     X, y, group, df = pre_process(input_filename)
     
     accuracies, best_params = classify_loso_model_selection(X, y, group, gs)
 
     # Create the files and save them
-    save_model(gs, 'trained_gs.pickle')
+    save_model(gs, gs_filename)
 
-    accuracy_file = open('accuracies_result.pickle', 'ab')
+    accuracy_file = open(acc_filename, 'ab')
     pickle.dump(accuracies, accuracy_file)
     accuracy_file.close()
 
-    best_params_file = open('best_params.pickle', 'ab')
+    best_params_file = open(best_params_filename, 'ab')
     pickle.dump(best_params, best_params_file)
     best_params_file.close()
